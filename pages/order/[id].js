3828ed3d789a224fef57fe6ba2d99582443f2298
SpicyArepa <infantevladimir@gmail.com>
2023-01-27 19:50:37 -0400

    added order details page

--- pages/order/[id].js
+++ pages/order/[id].js
@@ -1,158 +1,132 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import { useEffect, useReducer } from 'react';
-import Layout from '../../components/Layout';
-import { getData } from '../../lib/utils';
-
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
+import { getData } from "../../lib/utils";
 
 const OrderScreen = () => {
-  // order/:id
-  const { query } = useRouter();
-  const orderId = query.id;
+	// order/:id
+	const [isLoading, setIsLoading] = useState(true);
+	const [order, setOrder] = useState({});
+	const { query } = useRouter();
+	const orderId = query.id;
+  const regex = /T.*/i
 
-  useEffect(() => {
-    const fetchOrder = async () => {
-      try {
-        const res  = await getData(`/api/orders/${orderId}`);
-        if (res.error) {
-            console.log(res.error);
-            return;
-        } else {
-          const data = await res.json()
-            console.log(data)
-        }
-      } catch (err) {
-        console.log(err);;
-      }
-    };
-    if (!order._id || (order._id && order._id !== orderId)) {
-      fetchOrder();
-    }
-  }, [order, orderId]);
-  const {
-    shippingAddress,
-    paymentMethod,
-    orderItems,
-    itemsPrice,
-    taxPrice,
-    shippingPrice,
-    totalPrice,
-    isPaid,
-    paidAt,
-    isDelivered,
-    deliveredAt,
-  } = order;
+	useEffect(() => {
+		const fetchOrder = async () => {
+			try {
+				const res = await getData(`/api/orders/${orderId}`);
+				if (res.error) {
+					console.log(res.error);
+					return;
+				} else {
+					const data = await res.json();
+					console.log(data);
+					setIsLoading(false);
+					setOrder(data);
+				}
+			} catch (err) {
+				console.log(err);
+			}
+		};
+		if (!order._id || (order._id && order._id !== orderId)) {
+			fetchOrder();
+		}
+	}, [order, orderId]);
+	const {
+		shippingAddress,
+		orderItems,
+		itemsPrice,
+		shippingPrice,
+		totalPrice,
+		paidAt,
+	} = order;
 
-  return (
-    <Layout title={`Order ${orderId}`}>
-      <h1 className="mb-4 text-xl">{`Order ${orderId}`}</h1>
-      {loading ? (
-        <div>Loading...</div>
-      ) : error ? (
-        <div className="alert-error">{error}</div>
-      ) : (
-        <div className="grid md:grid-cols-4 md:gap-5">
-          <div className="overflow-x-auto md:col-span-3">
-            <div className="card  p-5">
-              <h2 className="mb-2 text-lg">Shipping Address</h2>
-              <div>
-                {shippingAddress.fullName}, {shippingAddress.address},{' '}
-                {shippingAddress.city}, {shippingAddress.postalCode},{' '}
-                {shippingAddress.country}
-              </div>
-              {isDelivered ? (
-                <div className="alert-success">Delivered at {deliveredAt}</div>
-              ) : (
-                <div className="alert-error">Not delivered</div>
-              )}
-            </div>
+	return (
+		<div className="order-details-container">
+			<h1 className="order-details-title">{`Order Details`}</h1>
+			<h2 className="order-details-id">{`ID: ${orderId}`}</h2>
+			{isLoading ? (
+				<div>Loading...</div>
+			) : (
+				<>
+					<h2 className="order-details-subtitle">Shipping Address</h2>
+					<p className="order-details-address">
+						{shippingAddress.fullName}, {shippingAddress.address}, {shippingAddress.city}, {shippingAddress.postalCode}, {shippingAddress.country}
+					</p>
 
-            <div className="card p-5">
-              <h2 className="mb-2 text-lg">Payment Method</h2>
-              <div>{paymentMethod}</div>
-              {isPaid ? (
-                <div className="alert-success">Paid at {paidAt}</div>
-              ) : (
-                <div className="alert-error">Not paid</div>
-              )}
-            </div>
 
-            <div className="card overflow-x-auto p-5">
-              <h2 className="mb-2 text-lg">Order Items</h2>
-              <table className="min-w-full">
-                <thead className="border-b">
-                  <tr>
-                    <th className="px-5 text-left">Item</th>
-                    <th className="    p-5 text-right">Quantity</th>
-                    <th className="  p-5 text-right">Price</th>
-                    <th className="p-5 text-right">Subtotal</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {orderItems.map((item) => (
-                    <tr key={item._id} className="border-b">
-                      <td>
-                        <Link href={`/product/${item.slug}`}>
-                          <a className="flex items-center">
-                            <Image
-                              src={item.image}
-                              alt={item.name}
-                              width={50}
-                              height={50}
-                            ></Image>
-                            &nbsp;
-                            {item.name}
-                          </a>
-                        </Link>
-                      </td>
-                      <td className=" p-5 text-right">{item.quantity}</td>
-                      <td className="p-5 text-right">${item.price}</td>
-                      <td className="p-5 text-right">
-                        ${item.quantity * item.price}
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
+						<h2 className="order-details-subtitle">Payment</h2>
+						<div className="order-details-payment">Paid with Stripe at {paidAt.replace(regex,"")}</div>
+
+
+					<h2 className="order-details-subtitle">Order Items</h2>
+          <div className="order-details-table-container">
+					<table className="order-details-table">
+						<thead className="order-details-table-head">
+							<tr>
+								<th className="order-details-table-column-title">Item</th>
+								<th className="order-details-table-column-title">Quantity</th>
+								<th className="order-details-table-column-title">Price</th>
+								<th className="order-details-table-column-title">Subtotal</th>
+							</tr>
+						</thead>
+						<tbody className="order-details-table-body">
+							{orderItems.map((item) => (
+								<tr key={item._id} className="border-b">
+									<td>
+										<a className="order-details-image-container">
+											<img
+												src={item.image}
+												alt={item.name}
+												width={50}
+												height={50}
+											/>
+											&nbsp;
+											{item.name}
+										</a>
+									</td>
+									<td className="order-details-table-column-item">
+										{item.quantity}
+									</td>
+									<td className="order-details-table-column-item">
+										${item.price}
+									</td>
+									<td className="order-details-table-column-item">
+										${item.quantity * item.price}
+									</td>
+								</tr>
+							))}
+						</tbody>
+					</table>
           </div>
-          <div>
-            <div className="card  p-5">
-              <h2 className="mb-2 text-lg">Order Summary</h2>
-              <ul>
-                <li>
-                  <div className="mb-2 flex justify-between">
-                    <div>Items</div>
-                    <div>${itemsPrice}</div>
-                  </div>
-                </li>{' '}
-                <li>
-                  <div className="mb-2 flex justify-between">
-                    <div>Tax</div>
-                    <div>${taxPrice}</div>
-                  </div>
-                </li>
-                <li>
-                  <div className="mb-2 flex justify-between">
-                    <div>Shipping</div>
-                    <div>${shippingPrice}</div>
-                  </div>
-                </li>
-                <li>
-                  <div className="mb-2 flex justify-between">
-                    <div>Total</div>
-                    <div>${totalPrice}</div>
-                  </div>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      )}
-    </Layout>
-  );
-}
+
+					<div className="order-details-card">
+						<h2 className="order-details-subtitle">Order Summary</h2>
+						<ul className="order-details-summary">
+							<li>
+								<div className="order-details-summary-item">
+									<div>Items</div>
+									<div>${itemsPrice}</div>
+								</div>
+							</li>
+							<li>
+								<div className="order-details-summary-item">
+									<div>Shipping</div>
+									<div>${shippingPrice}</div>
+								</div>
+							</li>
+							<li>
+								<div className="order-details-summary-item bold">
+									<div>Total</div>
+									<div>${totalPrice}</div>
+								</div>
+							</li>
+						</ul>
+					</div>
+				</>
+			)}
+		</div>
+	);
+};
 
 OrderScreen.auth = true;
 export default OrderScreen;
